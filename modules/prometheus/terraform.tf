0b814fbb13e6281ef14ea394dd7cd7fa73fc2710
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-02-16 19:27:23 +0100

    Update: Terraform and provider versions
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/prometheus/terraform.tf
+++ modules/prometheus/terraform.tf
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 0.14.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.14.0"
+      version = ">= 3.14.0"
     }
   }
 }
