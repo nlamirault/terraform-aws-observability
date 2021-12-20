69012635f3a1d4373fc5da667a854fb1eba1e3c0
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-12-20 12:36:58 +0100

    Add: outputs
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/amp/outputs.tf
+++ modules/amp/outputs.tf
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+output "amp_endpoint" {
+  value = aws_prometheus_workspace.amp.prometheus_endpoint
+}
+
+output "amp_id" {
+  value = aws_prometheus_workspace.amp.id
+}
