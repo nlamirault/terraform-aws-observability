1b46cff8fbce38460bce230b786d6886d6ae1a4b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-10-07 11:01:21 +0200

    Add: missing file
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ modules/amg/amg.tf
@@ -0,0 +1,43 @@
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+module "managed_grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "1.4.0"
+
+  # Workspace
+  name                      = local.service_name
+  description               = local.description
+  account_access_type       = "CURRENT_ACCOUNT"
+  authentication_providers  = ["AWS_SSO"]
+  permission_type           = "SERVICE_MANAGED"
+  data_sources              = ["CLOUDWATCH", "PROMETHEUS", "XRAY"]
+  notification_destinations = ["SNS"]
+
+  # Workspace IAM role
+  create_iam_role                = true
+  iam_role_name                  = local.role_name
+  use_iam_role_name_prefix       = true
+  iam_role_description           = local.description
+  iam_role_path                  = "/grafana/"
+  iam_role_force_detach_policies = true
+  iam_role_max_session_duration  = 7200
+  iam_role_tags                  = { role = true }
+
+  tags = merge(
+    { "Name" = format("%s-amg", local.service_name) },
+    local.tags
+  )
+}
