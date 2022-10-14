b8725c4594cd004e502a480bbf5bf5b3f578d571
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-10-14 18:37:17 +0200

    Update: license header
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/scripts/terraform-validate.sh
+++ hack/scripts/terraform-validate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2021 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
