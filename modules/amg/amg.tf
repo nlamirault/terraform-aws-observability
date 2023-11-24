8d6c18ee9863bc16302091b504bc4c1c99204657
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2023-11-24 17:19:06 +0100

    Add: license header
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/amg/amg.tf
+++ modules/amg/amg.tf
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 #tfsec:ignore:aws-iam-no-policy-wildcards
 module "managed_grafana" {
