0778539fe8105f3eb7a605fd223d3587d70e8d0c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2024-08-28 10:56:47 +0200

    feat(tempo): remove logging bucket
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/tempo/bucket.tf
+++ modules/tempo/bucket.tf
@@ -14,46 +14,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-module "buckets_logging" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.1"
-
-  for_each = local.buckets_names
-
-  bucket                  = format("%s-%s-logging", local.service_name, each.value)
-  block_public_acls       = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-  ignore_public_acls      = true
-
-  force_destroy = true
-
-  tags = merge(
-    { "Name" = format("%s-%s-logging", local.service_name, each.value) },
-    var.tags
-  )
-
-  versioning = {
-    enabled = true
-  }
-
-  server_side_encryption_configuration = var.enable_kms ? {
-    rule = {
-      bucket_key_enabled = true
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.tempo[0].arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  } : {}
-}
-
 #tfsec:ignore:aws-s3-encryption-customer-key
 module "buckets_data" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.1"
 
-  for_each = local.buckets_names
+  for_each = toset(local.buckets_names)
 
   bucket                  = format("%s-%s", local.service_name, each.value)
   block_public_acls       = true
@@ -63,16 +29,6 @@ module "buckets_data" {
 
   force_destroy = true
 
-  tags = merge(
-    { "Name" = format("%s-%s", local.service_name, each.value) },
-    var.tags
-  )
-
-  logging = {
-    target_bucket = module.buckets_logging[format("%s-%s", local.service_name, each.value)].s3_bucket_id
-    target_prefix = "log/"
-  }
-
   versioning = {
     enabled = true
   }
@@ -86,4 +42,9 @@ module "buckets_data" {
       }
     }
   } : {}
+
+  tags = merge(
+    { "Name" = format("%s-%s", local.service_name, each.value) },
+    var.tags
+  )
 }
--- modules/tempo/iam.tf
+++ modules/tempo/iam.tf
@@ -98,7 +98,7 @@ module "irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.44.0"
 
-  for_each = var.enable_irsa ? [1] : []
+  for_each = var.enable_irsa ? toset(["1"]) : toset([])
 
   create_role      = true
   role_description = "Role for Tempo"
@@ -121,7 +121,7 @@ module "pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "1.4.0"
 
-  for_each = var.enable_pod_identity ? [1] : []
+  for_each = var.enable_pod_identity ? toset(["1"]) : toset([])
 
   name = local.role_name
 
@@ -135,7 +135,7 @@ module "pod_identity" {
 
   associations = {
     main = {
-      cluster_name    = data.aws_eks_cluster.cluster_name
+      cluster_name    = data.aws_eks_cluster.this.id
       namespace       = var.namespace
       service_account = var.service_account
     }
