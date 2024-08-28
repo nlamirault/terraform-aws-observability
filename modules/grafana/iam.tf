d75197124998602025c0e8c696324653368df50a
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2024-08-28 10:55:21 +0200

    fix(grafana): terraform syntax and cluster name
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/grafana/iam.tf
+++ modules/grafana/iam.tf
@@ -18,7 +18,7 @@ module "irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.44.0"
 
-  for_each = var.enable_irsa ? [1] : []
+  for_each = var.enable_irsa ? toset(["1"]) : toset([])
 
   create_role      = true
   role_description = "Role for Grafana"
@@ -44,7 +44,7 @@ module "pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "1.4.0"
 
-  for_each = var.enable_pod_identity ? [1] : []
+  for_each = var.enable_pod_identity ? toset(["1"]) : toset([])
 
   name = local.role_name
 
@@ -57,7 +57,7 @@ module "pod_identity" {
 
   associations = {
     main = {
-      cluster_name    = data.aws_eks_cluster.cluster_name
+      cluster_name    = data.aws_eks_cluster.this.id
       namespace       = var.namespace
       service_account = var.service_account
     }
