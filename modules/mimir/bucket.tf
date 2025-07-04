08aaed5c1bd6b694eb2fb23d494f0050c1d90a31
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-07-04 12:36:24 +0200

    fix: bucket key
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- modules/mimir/bucket.tf
+++ modules/mimir/bucket.tf
@@ -30,6 +30,7 @@ module "buckets_data" {
     }
     } : {
     rule = {
+      bucket_key_enabled = true
       apply_server_side_encryption_by_default = {
         sse_algorithm = "AES256"
       }
